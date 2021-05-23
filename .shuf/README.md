47cd4809cc657abe081e37f52b794e028962be64
ValleyKnight <ValleyKnight@protonmail.com>
2021-05-22 22:44:58 -0400

    add mpv support to .shuf

--- .shuf/README.md
+++ .shuf/README.md
@@ -2,3 +2,7 @@
 
 To install, use `./install mode` where `mode` is either `debug` or `release`.
 Binary will be at `target/mode/.shuf`.
+
+Uses: 
+- `.shuf` to list a random entry in the current dir
+- `.shuf mpv` to play the random item in mpv
--- .shuf/src/main.rs
+++ .shuf/src/main.rs
@@ -4,6 +4,17 @@ use std::fs;
 fn main() {
     let paths = fs::read_dir(".").unwrap();
     let mut rng = rand::thread_rng();
-    let output = paths.choose(&mut rng).unwrap();
-    println!("{}", output.unwrap().file_name().into_string().unwrap());
+    let output = paths.choose(&mut rng).unwrap().unwrap().file_name().into_string().unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    
+    if args[1] == "mpv" {
+        std::process::Command::new("mpv")
+            .arg(output)
+            .stdin(std::process::Stdio::inherit())
+            .stdout(std::process::Stdio::inherit())
+            .output()
+            .expect("Could not play the the image, video, or directory in mpv.");
+    } else {
+        println!("{}", output);
+    }
 }
